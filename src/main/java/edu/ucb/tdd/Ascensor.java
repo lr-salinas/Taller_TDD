e124d3759a25e29f737fa779a1bec3af90dd08fb
lr-salinas <laura.salinas.r@ucb.edu.bo>
2021-05-03 22:27:23 -0400

    adding code

--- src/main/java/edu/ucb/tdd/Ascensor.java
+++ src/main/java/edu/ucb/tdd/Ascensor.java
@@ -2,10 +2,31 @@ package edu.ucb.tdd;
 
 public class Ascensor {
 
+    private Character [][] ascensor = {{'3'},
+                                       {'2'},
+                                       {'1'}};
+
+    private final int OCUPADO =1;
 
     public String verificar(String  verificando) throws Exception {
-
-
-        return "0";
+        return "1";
     }
+
+
+     public String direccion(int x) throws Exception{
+        verificarAscensor(x);
+
+         return null;
+     }
+
+    private void verificarAscensor(int ascen) throws Exception{
+        if(ascen < OCUPADO){
+            throw new Exception(ascen+"El ascensor esta ocupado");
+        }else{
+            System.out.println("El ascensor esta libre");
+        }
+    }
+
+
+
 }
--- src/test/java/edu/ucb/tdd/AscensorTest.java
+++ src/test/java/edu/ucb/tdd/AscensorTest.java
@@ -7,17 +7,36 @@ import org.junit.rules.ExpectedException;
 
 public class AscensorTest {
 
+    private Ascensor ascensor;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
     public void CrearPersona() throws Exception{
 
         //Preparacion
         Ascensor person = new Ascensor();
-
         //Logica de la Prueba
-        String  resultado = person.verificar("0");
-
+        String  resultado = person.verificar("1");
         //Verificacion o Assert
-        assertEquals("O", resultado);
+        assertEquals("1", resultado);
+    }
+
+    @Test
+    public void cuandoAscensorOcupadolanzarExcepcion() throws Exception{
+
+        ascensor.direccion(1);//persona1
+        exception.expect(Exception.class);
+        ascensor.direccion(1);//persona2
+    }
+
+    @Test
+    public void colocarPersonaEnPiso() throws Exception{
+        //preparacion de la prueba
+        //logica de la prueba
+        //verificacion o assert
+
     }
 
 }
