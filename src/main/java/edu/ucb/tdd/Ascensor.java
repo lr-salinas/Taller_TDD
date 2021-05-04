ede907ef275a3e965a38395210416cfbeb23b32e
coriccf <corina.colque.f@gmail.com>
2021-05-03 20:54:39 -0400

    First test

--- src/main/java/edu/ucb/tdd/Ascensor.java
+++ src/main/java/edu/ucb/tdd/Ascensor.java
@@ -1,4 +1,11 @@
 package edu.ucb.tdd;
 
 public class Ascensor {
+
+
+    public String verificar(String  verificando) throws Exception {
+
+
+        return "0";
+    }
 }
--- src/test/java/edu/ucb/tdd/AscensorTest.java
+++ src/test/java/edu/ucb/tdd/AscensorTest.java
@@ -1,4 +1,24 @@
 package edu.ucb.tdd;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
 public class AscensorTest {
+
+    @Test
+    public void CrearPersona() throws Exception{
+
+        //Preparacion
+        Ascensor person = new Ascensor();
+
+        //Logica de la Prueba
+        String  resultado = person.verificar("0");
+
+        //Verificacion o Assert
+        assertEquals("O", resultado);
+    }
+
 }
+
